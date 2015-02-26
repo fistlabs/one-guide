f8e96fa2f12987995260756c83cfc2fef2019f4b
spacy <spacy@yandex-team.ru>
2015-02-26 20:05:34 +0300

    Add an ability to override config

--- test/lib.one-guide.js
+++ test/lib.one-guide.js
@@ -77,6 +77,35 @@ describe('lib/one-guide', function () {
                 foo: 'bar'
             });
         });
+        it('Should have an ability to override adapter config', function () {
+            var guide = new OneGuide({
+                override: {
+                    base: {
+                        bar: 'baz'
+                    }
+                }
+            });
+            var baseLoad = require('../lib/base-adapter').loadConfig;
+
+            require('../lib/base-adapter').loadConfig = function (configFile) {
+                if (configFile === path.join(__dirname, '../lib/configs/yandex-node/.baserc')) {
+                    return {
+                        foo: 'bar'
+                    };
+                }
+            };
+
+            guide.addAdapter({
+                Class: require.resolve('../lib/base-adapter')
+            });
+            require('../lib/base-adapter').loadConfig = baseLoad;
+            assert.strictEqual(guide.adapters.length, 3);
+            assert.ok(guide.adapters[2] instanceof BaseAdapter);
+            assert.deepEqual(guide.adapters[2].config, {
+                foo: 'bar',
+                bar: 'baz'
+            });
+        });
     });
     describe('guide.isFilenameExcluded()', function () {
         it('Should return true if the given filename matches at least one exclude', function () {
